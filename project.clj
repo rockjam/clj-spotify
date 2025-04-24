e292367c4fa16e048ba4a3f77750c4ed9db81dc0
Nikolai Tatarinov <5min4eq.unity@gmail.com>
2025-04-24 11:55:32 +0200

    exclude dev dependencies from dependencies; add pom

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A client library for the Spotify Web API"
   :url "https://github.com/blmstrm/clj-spotify"
   :license {:name "The MIT License (MIT)"
-            :url "http://opensource.org/licenses/MIT"}
+            :url  "http://opensource.org/licenses/MIT"}
   :plugins [[lein-cloverage "1.2.1"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
@@ -10,12 +10,10 @@
                  [cheshire "5.10.0"]
                  [clj-http "3.11.0"]]
   :profiles {
-             :dev [:project/dev]
-             :test [:project/dev :profiles/test]
-             :project/dev {
-                           :source-paths ["dev-resources"]
-                           :dependencies [[org.clojure/tools.namespace "1.0.0"]
-                                          [loudmoauth "0.1.3"]
-                                          [ring "1.8.2"]
-                                          [org.clojure/tools.nrepl "0.2.13"]]}
-             :profiles/test {}})
+             :dev {
+                   :source-paths ["dev-resources"]
+                   :dependencies [[org.clojure/tools.namespace "1.0.0"]
+                                  [loudmoauth "0.1.3"]
+                                  [ring "1.8.2"]
+                                  [org.clojure/tools.nrepl "0.2.13"]]}})
+
